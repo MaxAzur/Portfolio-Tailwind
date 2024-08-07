ab469e42b6bf68398d10ac80db1c4b0871b48a0d
unknown <maxazur4@gmail.com>
2024-08-07 16:46:18 +0700

    fixed bug contact

--- dist/js/script.js
+++ dist/js/script.js
@@ -56,3 +56,24 @@ if (
 } else {
   darkToggle.checked = false;
 }
+
+//Contact Me
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const url = e.target.action;
+  const formData = new FormData(contactForm);
+
+  fetch(url, {
+    method: "POST",
+    body: formData,
+    mode: "no-cors",
+  })
+    .then(() => {
+      //url thankyou
+      window.location.href = "/dist/thankyou.html";
+    })
+    .catch((e) => alert("Error occured"));
+});
